78fd208f35590bda74d05b934c046f5bb8bd17a1
Michael Zhang <57410827+MichelDucartier@users.noreply.github.com>
2025-09-13 00:27:52 +0200

    Update and rename test.py to launch.py

--- launch.py
+++ launch.py
@@ -10,17 +10,6 @@ from src.usn.discord.credentials import DiscordCredentials
 
 logging.basicConfig(filename="notifier.log", level=logging.INFO)
 
-# status = CourseStatusRequester()
-# sesison_infos = status.request_url(url="https://sport.unil.ch/?pid=80&aid=58#content")
-# 
-# print(sesison_infos)
-
-# notifier = USNotifier(interval=0)
-# notifier.add_watch_url("https://sport.unil.ch/?pid=80&aid=58#content")
-# print(notifier.watched_urls)
-# 
-# notifier.loop()
-
 intents = discord.Intents.default()
 intents.message_content = True
 
